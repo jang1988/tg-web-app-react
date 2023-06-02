068cdb034524070e6c92bd20925d8355e543f841
Baklay Petro <pustota1988@gmail.com>
2023-06-02 20:23:59 +0300

    createForm&&addMainButton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import { useEffect } from 'react';
 import { useTelegram } from './hooks/useTelegram';
+import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
+import ProductList from './components/ProductList/ProductList';
+import Form from './components/Form/Form';
 
 function App() {
-    const { tg, onToggleButton } = useTelegram();
+    const { tg } = useTelegram();
 
     useEffect(() => {
         tg.ready();
@@ -13,7 +16,10 @@ function App() {
     return (
         <div className="App">
             <Header />
-            <button onClick={onToggleButton}>toggle</button>
+            <Routes>
+                <Route index element={<ProductList />} />
+                <Route path='form' element={<Form />} />
+            </Routes>
         </div>
     );
 }
