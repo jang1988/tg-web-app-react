d82c02f4b34d38ccbdd3359eeb7bc6fb0dd4935b
Baklay Petro <pustota1988@gmail.com>
2023-06-03 20:04:38 +0300

    fix

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://13.48.192.59:80/web-data', {
+        fetch('http://13.49.210.84:80/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
