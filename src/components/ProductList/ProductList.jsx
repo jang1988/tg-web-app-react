f2fb3abd845a6363f4c6d243d9814fd9ad76eafc
Baklay Petro <pustota1988@gmail.com>
2023-06-03 14:45:24 +0300

    fix

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,30 +1,29 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './ProductList.css';
-import ProductItem from '../ProductItem/ProductItem';
-import { useTelegram } from '../../hooks/useTelegram';
+import ProductItem from "../ProductItem/ProductItem";
+import {useTelegram} from "../../hooks/useTelegram";
 import {useCallback, useEffect} from "react";
 
 const products = [
-    { id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые' },
-    { id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая' },
-    { id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые' },
-    { id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая' },
-    { id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые' },
-    { id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая' },
-    { id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые' },
-    { id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая' },
-];
+    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
+    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
+    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
+    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+]
 
 const getTotalPrice = (items = []) => {
     return items.reduce((acc, item) => {
-        return (acc += item.price);
-    }, 0);
-};
+        return acc += item.price
+    }, 0)
+}
 
 const ProductList = () => {
     const [addedItems, setAddedItems] = useState([]);
-    const { tg, queryId } = useTelegram();
-    console.log('queryId: ', queryId)
+    const {tg, queryId} = useTelegram();
 
     const onSendData = useCallback(() => {
         const data = {
@@ -42,38 +41,42 @@ const ProductList = () => {
     }, [addedItems, queryId])
 
     useEffect(() => {
-        tg.onEvent('mainButtonClicked', onSendData);
+        tg.onEvent('mainButtonClicked', onSendData)
         return () => {
-            tg.offEvent('mainButtonClicked', onSendData);
-        };
-    }, [onSendData, tg]);
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData, tg])
 
     const onAdd = (product) => {
-        const alreadyAdded = addedItems.find((item) => item.id === product.id);
+        const alreadyAdded = addedItems.find(item => item.id === product.id);
         let newItems = [];
 
-        if (alreadyAdded) {
-            newItems = addedItems.filter((item) => item.id !== product.id);
+        if(alreadyAdded) {
+            newItems = addedItems.filter(item => item.id !== product.id);
         } else {
             newItems = [...addedItems, product];
         }
 
-        setAddedItems(newItems);
+        setAddedItems(newItems)
 
-        if (newItems.length === 0) {
+        if(newItems.length === 0) {
             tg.MainButton.hide();
         } else {
             tg.MainButton.show();
             tg.MainButton.setParams({
-                text: `Купить ${getTotalPrice(newItems)}`,
-            });
+                text: `Купить ${getTotalPrice(newItems)}`
+            })
         }
-    };
+    }
 
     return (
         <div className={'list'}>
-            {products.map((item, index) => (
-                <ProductItem key={index} product={item} onAdd={onAdd} className={'item'} />
+            {products.map(item => (
+                <ProductItem
+                    product={item}
+                    onAdd={onAdd}
+                    className={'item'}
+                />
             ))}
         </div>
     );
