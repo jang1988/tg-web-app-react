5c794066ab23e91c2403daaac1dbdc98de6e9d1b
Baklay Petro <pustota1988@gmail.com>
2023-06-03 21:10:54 +0300

    FIXURL

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://localhost:8000/web-data', {
+        fetch('http://13.49.210.84:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
